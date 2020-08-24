2146c05eea4cd9d564975a28bebfb978f7bf053e
rubicon44 <rub.b.c44@gmail.com>
2020-08-24 21:10:08 +0900

    display default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'goals#index'
+
+  get 'goals/index'
+  get 'goals/show'
+  get 'goals/new'
+  get 'goals/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
