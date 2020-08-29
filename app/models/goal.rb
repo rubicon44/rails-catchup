fe534e18305fef63b457e0932af5311b36e91254
rubicon44 <rub.b.c44@gmail.com>
2020-08-29 22:47:00 +0900

    delete unnecessary description in app/models/goal.rb

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -6,17 +6,4 @@ class Goal < ApplicationRecord
   # いいね機能
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-
-  # いいね機能用メソッド
-  def like(user)
-    likes.create(user_id: user.id)
-  end
-    
-  def unlike(user)
-    likes.find_by(user_id: user.id).destroy
-  end
-    
-  def like?(user)
-    liked_users.include?(user)
-  end
 end
