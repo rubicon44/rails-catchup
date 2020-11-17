424ef5117f35ffc4d6ec502fa583be999a9533f3
rubicon44 <rub.b.c44@gmail.com>
2020-11-17 12:26:13 +0900

    create Relationship Model, Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   # 認証（devise）
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_151944) do
+ActiveRecord::Schema.define(version: 2020_11_17_032332) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2020_10_28_151944) do
     t.index ["goal_id"], name: "index_notifications_on_goal_id"
     t.index ["visited_id"], name: "index_notifications_on_visited_id"
     t.index ["visitor_id"], name: "index_notifications_on_visitor_id"
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
