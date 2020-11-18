596654446fcd206a58bb4423313bff5fbeae4e78
rubicon44 <rub.b.c44@gmail.com>
2020-11-18 12:23:32 +0900

    create goal search function

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,7 +2,9 @@ class GoalsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @goals = Goal.all
+    # @goals = Goal.all
+    @search = Goal.ransack(params[:q])
+    @goals = @search.result
   end
 
   def show
