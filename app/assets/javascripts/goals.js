b1506578aa9ddd2ac1e13af02c88e4bbf0bdbb50
rubicon44 <rub.b.c44@gmail.com>
2020-12-17 22:42:01 +0900

    create comment modal

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -1,10 +1,24 @@
-// いいね切り替え
-// $(document).on('turbolinks:load', function () {
-//   $('.active-like a').click(function() {
-//     if ($(this).text() === 'いいね') {
-//       $(this).addClass('active');
-//     } else if ($(this).text() === 'いいねを取り消す') {
-//       $(this).removeClass('active');
-//     }
-//   });
-// });
+// comment
+var Grow = Grow || {};
+
+// show
+$(document).on('turbolinks:load', function () {
+  // コメントModal
+  Grow.modalControl = function() {
+    switchModal({
+      $modal: $('.js-modal'),
+      $modalOn: $('.js-modalOn'),
+      $modalBg: $('.js-modalBg')
+    });
+  };
+
+  function switchModal(param) {
+    param.$modalOn.on('click', function() {
+      param.$modalBg.fadeIn();
+      param.$modalBg.css('pointer-events','none');
+      param.$modal.fadeIn();
+    });
+  }
+
+  Grow.modalControl();
+});
