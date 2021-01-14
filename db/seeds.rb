9ddf541db9a8e9f884b59abb8d08c1d07dd764a8
rubicon44 <rub.b.c44@gmail.com>
2021-01-14 20:32:28 +0900

    modify seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,48 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 管理者ユーザー作成
-admin = User.create!(
-  username:  "admin",
-  email: "admin@admin.com",
-  password:  "adminadmin",
-  password_confirmation: 'adminadmin',
-  confirmed_at: Time.zone.now,
-  confirmation_sent_at: Time.zone.now,
-  admin: true
-)
-admin.avatar = open("#{Rails.root}/db/fixtures/admin.jpg")
-admin.save
-
 # ゲストユーザー作成
-guest = User.create!(
-  username: 'guest',
-  email: 'guest@example.com',
-  password: '123456',
-  password_confirmation: '123456',
-  confirmed_at: Time.zone.now,
-  confirmation_sent_at: Time.zone.now,
-  guest: true
+User.create!(
+               username: 'guest',
+               email: 'guest@example.com',
+               password: '123456',
+               password_confirmation: '123456',
+               confirmed_at: Time.zone.now,
+               confirmation_sent_at: Time.zone.now,
+               guest: true
 )
-guest.avatar = open("#{Rails.root}/db/fixtures/avatar-0.jpg")
-guest.save
 
-# テストユーザー作成
-user = User.create!(
-   username: 'testtest',
-   email: 'test@test.com',
-   password: 'testtest',
-   password_confirmation: 'testtest',
-   confirmed_at: Time.zone.now,
-   confirmation_sent_at: Time.zone.now
-)
-user.avatar = open("#{Rails.root}/db/fixtures/avatar-1.jpg")
-user.save
+1.upto(99) do |n|
+  username = "sample#{n}"
+  email = "sample-#{n}@example.com"
+  password = "123456"
+
+  User.create!(
+               username: username,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               confirmed_at: Time.zone.now,
+               confirmation_sent_at: Time.zone.now
+  )
+end
+
+users = User.order(:created_at).take(9)
+
+users.each_with_index do |user, n|
+  user.avatar = open("#{Rails.root}/db/fixtures/avatar-#{n}.jpg")
+  user.save
+end
+
+# 管理者ユーザー作成
+# admin = User.create!(
+#   username:  "admin",
+#   email: "admin@admin.com",
+#   password:  "adminadmin",
+#   password_confirmation: 'adminadmin',
+#   confirmed_at: Time.zone.now,
+#   confirmation_sent_at: Time.zone.now,
+#   admin: true
+# )
+# admin.avatar = open("#{Rails.root}/db/fixtures/admin.jpg")
+# admin.save
