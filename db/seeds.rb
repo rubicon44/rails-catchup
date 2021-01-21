3fce3a0859eeb199f78af0fea9cf8f42357aa0d3
rubicon44 <rub.b.c44@gmail.com>
2021-01-22 02:35:37 +0900

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Faker::Config.locale = :en
+
 # ゲストユーザー作成
 User.create!(
-               username: 'guest',
-               email: 'guest@example.com',
-               password: '123456',
-               password_confirmation: '123456',
-               confirmed_at: Time.zone.now,
-               confirmation_sent_at: Time.zone.now,
-               guest: true
+             username: 'guest',
+             email: 'guest@example.com',
+             password: '123456',
+             password_confirmation: '123456',
+             confirmed_at: Time.zone.now,
+             confirmation_sent_at: Time.zone.now,
+             guest: true
 )
 
 1.upto(10) do |n|
@@ -51,3 +53,137 @@ admin = User.create!(
 )
 admin.avatar = open("#{Rails.root}/db/fixtures/admin.jpg")
 admin.save
+
+# タスク登録
+1.upto(1) do |n|
+  users[1].goals.create!(
+    name: "Railsを極める",
+    description: "Railsを極める",
+    status: 2,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+
+  users[1].goals.create!(
+    name: "Web系自社開発企業に転職する",
+    description: "Web系自社開発企業に転職する",
+    status: 1,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+
+  users[1].goals.create!(
+    name: "フリーランスになる",
+    description: "フリーランスになる",
+    status: 0,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+1.upto(1) do |n|
+  users[2].goals.create!(
+    name: "Reactを極める",
+    description: "Reactを極める",
+    status: 3,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+
+  users[2].goals.create!(
+    name: "受託制作企業に転職する",
+    description: "受託制作企業に転職する",
+    status: 2,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+
+  users[2].goals.create!(
+    name: "Webディレクターになる",
+    description: "Webディレクターになる",
+    status: 1,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+
+1.upto(1) do |n|
+  name = "HTMLを極める"
+  description = "HTMLを極める"
+  status = 2
+
+  users[3].goals.create!(
+    name: name,
+    description: description,
+    status: status,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+
+1.upto(1) do |n|
+  name = "CSSを極める"
+  description = "CSSを極める"
+  status = 3
+
+  users[4].goals.create!(
+    name: name,
+    description: description,
+    status: status,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+
+1.upto(1) do |n|
+  name = "AWSを極める"
+  description = "AWSを極める"
+  status = 0
+
+  users[5].goals.create!(
+    name: name,
+    description: description,
+    status: status,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+1.upto(1) do |n|
+  name = "GCPを極める"
+  description = "GCPを極める"
+  status = 1
+
+  users[6].goals.create!(
+    name: name,
+    description: description,
+    status: status,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+1.upto(1) do |n|
+  name = "Firebaseを極める"
+  description = "Firebaseを極める"
+  status = 2
+
+  users[7].goals.create!(
+    name: name,
+    description: description,
+    status: status,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+1.upto(1) do |n|
+  name = "GatsbyJSを極める"
+  description = "GatsbyJSを極める"
+  status = 3
+
+  users[8].goals.create!(
+    name: name,
+    description: description,
+    status: status,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+# いいね
+
+# コメント
+
+# リレーションシップ
