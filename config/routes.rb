b6f48502811964c78e0dabf7c4f9cfb1d42d2f2b
rubicon44 <rub.b.c44@gmail.com>
2021-01-31 04:54:46 +0900

    modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index] do
-    get 'search', on: :collection
+    get :search, on: :collection
   end
 
   resources :users, only: [:show] do
